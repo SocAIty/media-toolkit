89c87215efb8f240448ba1d969423f28583f8305
w4hns1nn <myballs@dontwanttoshare.com>
2025-02-05 17:09:08 +0100

    security update

--- media_toolkit/utils/file_conversion.py
+++ media_toolkit/utils/file_conversion.py
@@ -63,30 +63,36 @@ def media_from_any(file, media_file_type=None, use_temp_file: bool = False, temp
     return media_file_instance
 
 
-def media_from_file_result(file_result: dict):
+def media_from_file_result(file_result: dict, allow_reads_from_disk: bool = False) -> MediaFile:
     """
-    Converts a file result to a MediaFile.
+    Converts a file result to a MediaFile. FileResult contains "content_type", "content" and "file_name".
+    This type stems usually from a FastTaskAPI JobResult.
     :param file_result: The file result to convert.
+    :param allow_reads_from_disk: If True, the file will be read from disk if the content is a file path.
+        This is in most cases not recommended, because it can be a security risk.
     :return: The MediaFile.
     """
     content_type = file_result.get("content_type", None)
-    if content_type is None:
-        content = file_result.get('content', file_result)
-        return MediaFile().from_any(content)
+    target_class = MediaFile
+    if content_type is not None and isinstance(content_type, str):
+        # determine target class from file type
+        content_type = content_type.lower()
+        file_types = {
+            "octet-stream": MediaFile,
+            "image": ImageFile,
+            "audio_file": AudioFile,
+            "video": VideoFile
+        }
+        # fancy way to write efficient the factory conversion
+        target_class = next(
+            filter(lambda ft: ft[0] in content_type, file_types.items()),
+            (None, MediaFile) # return tuple as default because next returns key, value
+        )[1]
 
-    # determine target class from file type
-    content_type = content_type.lower()
-    file_types = {
-        "octet-stream": MediaFile,
-        "image": ImageFile,
-        "audio_file": AudioFile,
-        "video": VideoFile
-    }
-    # fancy way to write efficient the factory conversion
-    target_class = next(
-        filter(lambda ft: ft[0] in content_type, file_types.items()),
-        (None, MediaFile) # return tuple as default because next returns key, value
-    )[1]
+    content = file_result.get('content', file_result)
+    # make sure that file_result is not a file path of system to avoid security issues
+    if not allow_reads_from_disk and MediaFile._is_valid_file_path(content):
+        raise ValueError("Reading files from disk is not allowed. This can be a security risk.")
 
     return target_class().from_dict(file_result)
 
